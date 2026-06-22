709e89382aa97c6da18306bb8691a5d569fe1ae9
alirezashojaian1987 <alirezashojaian22@gmail.com>
2026-06-22 14:21:47 +0330

    Hero section added for the homepage and designed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import MainLayout from "@/components/layout/MainLayout/MainLayout";
 
+import Hero from "@/components/Home/Hero/Hero";
+
 export default function HomePage(){
   return (
     <MainLayout>
-      <></>
+      <Hero/>
     </MainLayout>
   );
 }
